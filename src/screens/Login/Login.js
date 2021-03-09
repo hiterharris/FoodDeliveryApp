536ef07b515ddbf70236dc9e8778b84353f66f25
hiterharris <hiterharris@comcast.net>
2021-03-08 21:33:42 -0500

    build login page

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { COLORS } from '../../constants';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { FONTS } from '../../constants'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Login = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <Text>Login</Text>
-            <Button title='Home' onPress={() => navigation.navigate("Home")} />
+            <Text style={styles.title}>Welcome!</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+            <TextInput style={styles.input} placeholder="Password" />
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Home")}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <Text style={styles.signup}>Sign Up</Text>
         </View>
   )
 }
@@ -15,9 +21,40 @@ const Login = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: COLORS.lightGray4,
-        marginTop: 48,
+        alignItems: 'center',
+        backgroundColor: '#00BAFF',
+        paddingTop: 48,
     },
+    title: {
+        ...FONTS.h1,
+        color: 'white',
+        padding: 20,
+    },
+    input: {
+        backgroundColor: 'white',
+        width: 250,
+        height: 50,
+        margin: 5,
+        borderRadius: 10,
+        padding: 10,
+    },
+    button: {
+        width: 150,
+        margin: 20,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'white',
+    },
+    buttonText: {
+        color: 'white',
+        ...FONTS.h3,
+        textAlign: 'center',
+        padding: 10,
+    },
+    signup: {
+        color: 'white',
+        ...FONTS.h4,
+    }
 })
 
 export default Login;
